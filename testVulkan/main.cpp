97af847db99faed34fab73f3b42e272c6f391a87
Shane Cheng <shane85cheng@gmail.com>
2024-06-05 15:54:28 -0500

    glslang build shader module from glsl

--- testVulkan/main.cpp
+++ testVulkan/main.cpp
@@ -8,10 +8,16 @@
 #include <algorithm>
 #include <iterator>
 #include <numeric>
+#include <filesystem> // for shader
 #define VMA_IMPLEMENTATION
 #include <vk_mem_alloc.h>
 #include <glslang/Include/glslang_c_interface.h>
-#include <glslang/Public/resource_limits_c.h>
+#include <glslang/Public/resource_limits_c.h> //c
+#include <glslang/Public/ResourceLimits.h>    //c++
+#include <glslang/Public/ShaderLang.h>
+#include <glslang/SPIRV/GlslangToSpv.h>
+
+#include <DirStackFileIncluder.h>
 
 #define ASSERT(expr, message) \
     {                         \
@@ -44,6 +50,13 @@ VkQueue gPresentationQueue = VK_NULL_HANDLE;
 VkQueue gSparseQueues = VK_NULL_HANDLE;
 VmaAllocator gVmaAllocator = VK_NULL_HANDLE;
 VkSwapchainKHR gSwapChain = VK_NULL_HANDLE;
+
+VkShaderModule gVertexShaderModule = VK_NULL_HANDLE;
+
+void createShaderModule(
+    const std::string &filePath,
+    const std::string &entryPoint,
+    const std::string &correlationId);
 
 static VkBool32 debugMessengerCallback(VkDebugUtilsMessageSeverityFlagBitsEXT severity,
                                        VkDebugUtilsMessageTypeFlagsEXT types,
@@ -720,7 +733,207 @@ void init()
     }
     // 17. shader module
     {
+        // lateral for filepath in modern c++
+        const auto shadersPath = std::filesystem::current_path() / "testVulkan/shaders";
+        const auto vertexShaderPath = shadersPath / "test.vert";
+        const auto fragShaderPath = shadersPath / "test.frag";
+        createShaderModule(
+            vertexShaderPath.string(),
+            "main",
+            "test.vert");
+        createShaderModule(
+            fragShaderPath.string(),
+            "main",
+            "test.frag");
     }
+}
+
+// 1. load spv as binary, easy
+// 2. build from glsl in the runtime; complicated
+// data: txt array
+// shader stage
+// shaderDir for include
+// entryPoint: main()
+// std::vector<char>: binary array
+std::vector<char> glslToSpirv(const std::vector<char> &shaderText,
+                              EShLanguage shaderStage,
+                              const std::string &shaderDir,
+                              const char *entryPoint)
+{
+    glslang::TShader tmp(shaderStage);
+    const char *data = shaderText.data();
+    // c style: array + size
+    tmp.setStrings(&data, 1);
+
+    glslang::EshTargetClientVersion clientVersion = glslang::EShTargetVulkan_1_3;
+    glslang::EShTargetLanguageVersion langVersion = glslang::EShTargetSpv_1_6;
+    // raytracing
+    if (shaderStage == EShLangRayGen || shaderStage == EShLangAnyHit ||
+        shaderStage == EShLangClosestHit || shaderStage == EShLangMiss)
+    {
+        langVersion = glslang::EShTargetSpv_1_4;
+    }
+
+    // use opengl 4.6
+    tmp.setEnvInput(glslang::EShSourceGlsl, shaderStage, glslang::EShClientVulkan, 460);
+    tmp.setEnvClient(glslang::EShClientVulkan, clientVersion);
+    tmp.setEnvTarget(glslang::EShTargetSpv, langVersion);
+    // // validate only
+    // tmp.setEnvClient(EShClientNone, 0);
+    // tmp.setEnvTarget(EShTargetNone, 0);
+    tmp.setEntryPoint(entryPoint);
+    tmp.setSourceEntryPoint(entryPoint);
+
+    // parsing include in the shader file
+    const TBuiltInResource *resources = GetDefaultResources();
+    // Message choices for what errors and warnings are given.
+    // https://chromium.googlesource.com/external/github.com/KhronosGroup/glslang/+/refs/heads/SPIR-V_1.4/glslang/Public/ShaderLang.h
+    EShMessages messages = (EShMessages)(EShMsgDefault | EShMsgSpvRules | EShMsgVulkanRules | EShMsgDebugInfo);
+
+    DirStackFileIncluder includer;
+    std::vector<std::string> IncludeDirectoryList;
+    std::for_each(IncludeDirectoryList.rbegin(), IncludeDirectoryList.rend(), [&includer](const std::string &dir)
+                  { includer.pushExternalLocalDirectory(dir); });
+    std::string preprocessedGLSL;
+    if (!tmp.preprocess(resources, 460, ENoProfile, false, false, messages,
+                        &preprocessedGLSL, includer))
+    {
+        std::cout << "Preprocessing failed for shader: " << std::endl;
+        std::cout << std::endl;
+        std::cout << tmp.getInfoLog() << std::endl;
+        std::cout << tmp.getInfoDebugLog() << std::endl;
+        ASSERT(false, "Error occured");
+        return std::vector<char>();
+    }
+
+    // preprocessedGLSL = removeUnnecessaryLines(preprocessedGLSL);
+
+    const char *preprocessedGLSLStr = preprocessedGLSL.c_str();
+
+    // without include
+    glslang::TShader tshader(shaderStage);
+    tshader.setEnvInput(glslang::EShSourceGlsl, shaderStage, glslang::EShClientVulkan, 460);
+    tshader.setEnvClient(glslang::EShClientVulkan, clientVersion);
+    tshader.setEnvTarget(glslang::EShTargetSpv, langVersion);
+    tshader.setEntryPoint(entryPoint);
+    tshader.setSourceEntryPoint(entryPoint);
+    tshader.setStrings(&preprocessedGLSLStr, 1);
+
+    if (!tshader.parse(resources, 460, false, messages))
+    {
+        std::cout << "Parsing failed for shader: " << std::endl;
+        std::cout << std::endl;
+        std::cout << tshader.getInfoLog() << std::endl;
+        std::cout << tshader.getInfoDebugLog() << std::endl;
+        ASSERT(false, "parse failed");
+        return std::vector<char>();
+    }
+
+    glslang::SpvOptions spvOptions;
+
+    // use debug settings
+    tshader.setDebugInfo(true);
+    spvOptions.emitNonSemanticShaderDebugInfo = true;
+    spvOptions.emitNonSemanticShaderDebugSource = true;
+    spvOptions.generateDebugInfo = true;
+    spvOptions.disableOptimizer = true;
+    spvOptions.optimizeSize = false;
+    spvOptions.stripDebugInfo = false;
+
+    glslang::TProgram program;
+    program.addShader(&tshader);
+    if (!program.link(messages))
+    {
+        std::cout << "Parsing failed for shader " << std::endl;
+        std::cout << program.getInfoLog() << std::endl;
+        std::cout << program.getInfoDebugLog() << std::endl;
+        ASSERT(false, "Failed to link shader stage to program");
+    }
+
+    std::vector<uint32_t> spirvArtifacts;
+    spv::SpvBuildLogger spvLogger;
+    // write result to
+    glslang::GlslangToSpv(*program.getIntermediate(shaderStage),
+                          spirvArtifacts,
+                          &spvLogger,
+                          &spvOptions);
+
+    std::vector<char> byteCode;
+    byteCode.resize(spirvArtifacts.size() * (sizeof(uint32_t) / sizeof(char)));
+    std::memcpy(byteCode.data(), spirvArtifacts.data(), byteCode.size());
+
+    return byteCode;
+}
+
+std::vector<char> readFile(const std::string &filePath, bool isBinary)
+{
+    std::ios_base::openmode mode = std::ios::ate;
+    if (isBinary)
+    {
+        mode |= std::ios::binary;
+    }
+    std::ifstream file(filePath, mode);
+    if (!file.is_open())
+    {
+        throw std::runtime_error("failed to open file!");
+    }
+
+    size_t fileSize = (size_t)file.tellg();
+    if (!isBinary)
+    {
+        fileSize += 1; // add extra for null char at end
+    }
+    std::vector<char> buffer(fileSize);
+    file.seekg(0);
+    file.read(reinterpret_cast<char *>(buffer.data()), fileSize);
+    file.close();
+    if (!isBinary)
+    {
+        buffer[buffer.size() - 1] = '\0';
+    }
+    return buffer;
+}
+
+EShLanguage shaderStageFromFileName(const std::filesystem::path &path)
+{
+    const auto ext = path.extension().string();
+    if (ext == ".vert")
+    {
+        return EShLangVertex;
+    }
+    else if (ext == ".frag")
+    {
+        return EShLangFragment;
+    }
+    else
+    {
+        ASSERT(false, "unsupported shader stage");
+    }
+}
+
+// const std::vector<char>& spirv,
+void createShaderModule(
+    const std::string &filePath,
+    const std::string &entryPoint,
+    const std::string &correlationId)
+{
+    const auto path = std::filesystem::path(filePath);
+    const bool isBinary = path.extension().string() == ".spv";
+    std::vector<char> data = readFile(filePath, isBinary);
+    if (!isBinary)
+    {
+        data = glslToSpirv(data,
+                           shaderStageFromFileName(path),
+                           path.parent_path().string(),
+                           entryPoint.c_str());
+    }
+    const VkShaderModuleCreateInfo shaderModule = {
+        .sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
+        .codeSize = data.size(),
+        .pCode = (const uint32_t *)data.data(),
+    };
+    VK_CHECK(vkCreateShaderModule(gLogicDevice, &shaderModule, nullptr, &gVertexShaderModule));
+    setCorrlationId(gVertexShaderModule, VK_OBJECT_TYPE_SHADER_MODULE, "Shader Module: " + correlationId);
 }
 
 int main(int argc, char **argv)
@@ -733,7 +946,9 @@ int main(int argc, char **argv)
     gWindow.init(&cfg);
 
     VK_CHECK(volkInitialize());
-    glslang_initialize_process();
+    // c api
+    // glslang_initialize_process();
+    glslang::InitializeProcess();
 
     init();
 
@@ -743,6 +958,7 @@ int main(int argc, char **argv)
     }
 
     gWindow.shutdown();
-    glslang_finalize_process();
+    // glslang_finalize_process();
+    glslang::FinalizeProcess();
     return 0;
 }
